18429d26bcc487a742f407e97b0adce6f61cd06a
Striletska Kateryna <striletskay2005@gmail.com>
2024-03-13 16:44:18 +0200

    Fourth commit: crated function

---
+++ app/io/input.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def read_from_console():
+    """
+    A function for reading the entered text from the console.
+    :return: str. The text that was entered.
+
+    """
+
+
+
+def read_from_file(inputFile):
+    """
+
+    :param inputFile:
+    :return: str. Text from file
+    """
+
+
+
+def read_from_file_pandas(inputFile):
+    """
+
+    :param inputFile:
+    :return:
+    """
---
+++ app/io/output.py
@@ -0,0 +1,16 @@
+
+def print_text(input):
+    """
+
+    :param input:
+    :return:
+    """
+
+
+def write_text_in_file(fileName, inputText):
+    """
+
+    :param fileName:
+    :param inputText:
+    :return:
+    """
