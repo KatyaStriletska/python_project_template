4e9d23b9fa4ff747545b41ac0a39bac07399a292
Striletska Kateryna <striletskay2005@gmail.com>
2024-03-19 00:25:55 +0200

    code structure improvement

--- app/io/input.py
+++ app/io/input.py
@@ -4,24 +4,31 @@ import pandas as pd
 def input_from_console():
     """
     A function for reading the entered text from the console.
+
+    :argument: This function hasn't any argument.
+
     :return: str. The text that was entered.
     """
     text = input("Please, enter your text: ")
     return str(text)
 
 
-def read_from_file(inputFile):
+def read_from_file(input_file):
     """
     This function for reading text from file with using standard python's methods.
     Note:
         If there is no specific file you will get an error about it.
-    :param inputFile:  It's file to read.
+
+    :argument: input_file:  It's file to read.
+
     :return: str. Text from file.
+
+    :raise: Function raise a "FileNotFoundError" error if there isn't particular file.
     """
 
     text = ""
     try:
-        with open(inputFile) as file:
+        with open(input_file) as file:
             file_content = file.read()
             text += file_content
     except FileNotFoundError as a:
@@ -29,17 +36,21 @@ def read_from_file(inputFile):
     return text
 
 
-def read_from_file_pandas(inputFile):
+def read_from_file_pandas(input_file):
     """
     This function for reading text from file with using the pandas library.
     Note:
         If there is no specific file you will get an error about it.
-    :param inputFile:  It's file to read.
+
+    :argument: input_file:  It's file to read.
+
     :return: str. Text from file.
+
+    :raise: Function raise a "FileNotFoundError" error if there isn't particular file.
     """
 
     try:
-        text = pd.read_csv(inputFile)
+        text = pd.read_csv(input_file)
     except FileNotFoundError as a:
         raise FileNotFoundError("File not found: ", a)
     return text
--- app/io/output.py
+++ app/io/output.py
@@ -1,23 +1,50 @@
-
-def print_text(input):
-    """
-    Function for enter text from the console
-    :return: This function returns text which input user from console.
-    examples:
-    """
-    print(input)
+import pandas as pd
 
 
-def write_text_in_file(inputText, fileName):
+def print_text(output_data):
     """
+    Function for entering text from the console. It printed the text, that was come as argument.
 
-    :param fileName:
-    :param inputText:
-    :return:
+    :argument: output_data(Any types). It's data to be printed in console.
+
+    :return: None: This function return nothing.
     """
-    try:
-        with open(fileName, 'w') as file:
-            file.write(inputText)
-    except FileNotFoundError as a:
-        print("File not found: ", a)
-    print(f"Input text was succeeded written in file: {fileName}!")
+    print(str(output_data))
+    return None
+
+
+def write_text_in_file(output_data, file_name):
+    """
+    Function to writing text in file. After the execution of this function, there will be a message on the console about
+    the successful completion of the action.
+
+    :argument:
+            output_data (Any types). It's data to be written in input file.
+            file_name (file). It's file in which will be written input data. If there isn't particular file, it will be
+                                created.
+
+    :return: None. This function returns nothing.
+    """
+    with open(file_name, 'w') as file:
+        file.write(output_data)
+
+    print(f"Input text was succeeded written in file: {file_name}!")
+    return None
+
+
+def write_text_in_file_pandas(output_data, file_name):
+    """
+    Function to writing text in file with extension 'csv' with using library pandas. After the execution of this
+    function, there will be a message on the console about the successful completion of the action.
+
+    :argument:
+            output_data (Any types). It's data to be written in input file.
+            file_name (file). It's file in which will be written input data. If there isn't particular file, it will be
+                                created. File must be with extension 'csv'!
+
+    :return: None. This function returns nothing.
+    """
+    df = pd.DataFrame(output_data)
+    df.to_csv(file_name)
+    print(f"Input text was succeeded written in file: {file_name}!")
+    return None
